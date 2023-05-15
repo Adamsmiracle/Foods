636f87e8b6efdcdcafeb979ef75d5639ad28d620
adams miracle <adamsmiracle0@gmail.com>
2023-05-15 16:47:39 +0000

    Final food site

---
+++ app.js
@@ -0,0 +1,23 @@
+// The functionality for the navbar
+document.querySelector(".menu").addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach((item) => {
+        item.classList.toggle("change")
+    })
+})
+
+// Functionality for the Landing page
+const icons = document.querySelectorAll(".section-1-icons i")
+let i = 1
+
+setInterval(_ => {
+    i++
+
+    const icon = document.querySelector('.section-1-icons .change')
+    icon.classList.remove("change")
+    if (i > icons.length) {
+        icons[0].classList.add("change")
+        i = 1
+    } else {
+        icon.nextElementSibling.classList.add("change")
+    }
+}, 4000)
